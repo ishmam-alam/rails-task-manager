6ca844619a354e3384109421f9b169eaaca9f3e7
ishmam alam <ishmam.alam01@gmail.com>
2023-05-23 15:04:04 +0200

    task model controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # list all tasks
+  get 'tasks', to: 'tasks#index'
+
+  get "tasks/:[id]", to: "tasks#show"
+
+  # Create / add new task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  # Update / edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks#id', to: 'tasks#update'
+
+  # view details of a task
+  get 'tasks/:id/details', to: 'task#details'
+
+  # Delete / remove task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
